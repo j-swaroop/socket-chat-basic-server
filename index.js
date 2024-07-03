21fba97f4106adcdf2e8d37bbaeaacc9cb7ecd37
j-swaroop <jyothiswaroop654@gmail.com>
2024-07-03 20:56:14 +0530

    nnnn

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 
 app.use(cors());
-app.use(
-  cors({
-    origin: "https://socket-chat-basic-client.vercel.app",
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", 'https://socket-chat-basic-client.vercel.app/');
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 const server = http.createServer(app);
 const io = new Server(server);
